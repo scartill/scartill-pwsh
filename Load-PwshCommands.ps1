af7588529d37a53c5a4a9524ab0b96612be55dc0
Boris Resnick <boris.resnick@gmail.com>
2025-06-09 08:31:38 +0300

    refactor(ps1): improve error handling and add config URL variable

--- Load-PwshCommands.ps1
+++ Load-PwshCommands.ps1
@@ -1,6 +1,9 @@
+$base_url = "https://gist.githubusercontent.com/scartill/"
+$config_url = "$base_url/5b703c57d71aaff4364552344f64959a/raw/pwsh-7-posh-conf.json"
+
 try {
-    # Enabling Oh-My-Posh
-    oh-my-posh --init --shell pwsh --config https://gist.githubusercontent.com/scartill/5b703c57d71aaff4364552344f64959a/raw/pwsh-7-posh-conf.json | Invoke-Expression
+    $posh_cmd = "oh-my-posh --init --shell pwsh --config $config_url"
+    Invoke-Expression $posh_cmd | Invoke-Expression
 } catch {
-    Write-Host "Error loading Oh-My-Posh"
+    Write-Error "Error loading Oh-My-Posh"
 }
