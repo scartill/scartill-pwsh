6457adcae70ff2a40fd8322f5627b9d22d0ebf40
Boris Resnick <boris.resnick@gmail.com>
2025-06-10 22:54:25 +0300

    refactor(config): update oh-my-posh config to use local file and add path segment

--- Load-PwshCommands.ps1
+++ Load-PwshCommands.ps1
@@ -1,7 +1,7 @@
-$config_url = "https://raw.githubusercontent.com/scartill/scartill-pwsh/refs/heads/main/on-my-posh-config.yaml"
+$config_file = "$PSScriptRoot/on-my-posh-config.yaml"
 
 try {
-    $posh_cmd = "oh-my-posh --init --shell pwsh --config $config_url"
+    $posh_cmd = "oh-my-posh --init --shell pwsh --config $config_file"
     Invoke-Expression $posh_cmd | Invoke-Expression
 } catch {
     Write-Error "Error loading Oh-My-Posh"
