f51df6097459bce12524886dbb54f639ab88ea2b
Boris Resnick <boris.resnick@gmail.com>
2025-06-10 18:28:17 +0300

    feat(theme): update oh-my-posh theme with AWS and Kotlin segments

--- Load-PwshCommands.ps1
+++ Load-PwshCommands.ps1
@@ -1,5 +1,5 @@
-$base_url = "https://gist.githubusercontent.com/scartill/"
-$config_url = "$base_url/5b703c57d71aaff4364552344f64959a/raw/pwsh-7-posh-conf.json"
+$base_url = "https://gist.githubusercontent.com/scartill"
+$config_url = "$base_url/5b703c57d71aaff4364552344f64959a/raw/9b4b72456153222e2629d451c5f015b32c7b9eb6/pwsh-7-posh-conf.json"
 
 try {
     $posh_cmd = "oh-my-posh --init --shell pwsh --config $config_url"
