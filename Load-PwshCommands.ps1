6afc0e74fc4022f8ddeac7f9204714b43d6c6074
Boris Resnick <boris.resnick@gmail.com>
2025-06-10 18:29:20 +0300

    refactor(config): update path to oh-my-posh config file

--- Load-PwshCommands.ps1
+++ Load-PwshCommands.ps1
@@ -1,5 +1,4 @@
-$base_url = "https://gist.githubusercontent.com/scartill"
-$config_url = "$base_url/5b703c57d71aaff4364552344f64959a/raw/9b4b72456153222e2629d451c5f015b32c7b9eb6/pwsh-7-posh-conf.json"
+$config_url = "https://raw.githubusercontent.com/scartill/scartill-pwsh/refs/heads/main/on-my-posh-config.yaml"
 
 try {
     $posh_cmd = "oh-my-posh --init --shell pwsh --config $config_url"
