bbae805817f16e2e49df41af4f1430ced8aaaf97
Boris Resnick <boris.resnick@gmail.com>
2025-06-12 14:27:42 +0300

    feat(theme): convert oh-my-posh config from yaml to json format

--- Load-PwshCommands.ps1
+++ Load-PwshCommands.ps1
@@ -1,4 +1,7 @@
-$config_file = "$PSScriptRoot/on-my-posh-config.yaml"
+# $config_file = "$PSScriptRoot/on-my-posh-config.yaml"
+$config_file = "${PSScriptRoot}/on-my-posh-config.json"
+
+Write-Host "Loading Oh-My-Posh from $config_file"
 
 try {
     $posh_cmd = "oh-my-posh --init --shell pwsh --config $config_file"
