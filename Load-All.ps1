fff73e852185d04eec1a95da832a6974975c8419
Boris Resnick <boris.resnick@gmail.com>
2025-07-25 18:40:23 +0300

    feat: add PROJECTS_HOME environment variable initialization

---
+++ Load-All.ps1
@@ -0,0 +1,9 @@
+. $PSScriptRoot/Load-GitCommands.ps1
+. $PSScriptRoot/Load-PwshCommands.ps1
+. $PSScriptRoot/Load-PyCommands.ps1
+. $PSScriptRoot/Load-AwsCommands.ps1
+. $PSScriptRoot/Load-UtilCommands.ps1
+. $PSScriptRoot/Init-Choco.ps1
+
+# General Common
+$env:PROJECTS_HOME = "${env:HOMEDRIVE}${env:HOMEPATH}/projects"
