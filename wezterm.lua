cdafd3b99b109554f33175a4958559b3957a456d
Boris Resnick <boris.resnick@gmail.com>
2025-09-19 20:57:50 +0300

    refactor: simplify wezterm workspace setup and update decorations

--- wezterm.lua
+++ wezterm.lua
@@ -9,41 +9,24 @@ wezterm.on("gui-startup", function(cmd)
   local safir = fvc .. "/safir"
 
   if ws == "safir" then
-    local tab_fusion, pane1, win = mux.spawn_window {
+    local tab_fusion, pane_fusion, win = mux.spawn_window {
         workspace = ws,
         cwd = safir .. "/safir-fusion",
         args = { "pwsh.exe" },
     }
     tab_fusion:set_title("fusion")
-
-    local tab_sub, pane_sub = win:spawn_tab {
-        workspace = ws,
-        cwd = "scripts",
-        args = {"pwsh.exe" },
-    }
+    local tab_sub, pane_sub = win:spawn_tab { workspace = ws }
     tab_sub:set_title("tracks")
-    -- pane_sub:send_text("./SubsribeTo-Tracks.ps1\n")
-
-    local tab3, pane3 = win:spawn_tab {
-        workspace = ws,
-        cwd = safir .. "/safir-fusion/scripts",
-        args = { "pwsh.exe" },
-    }
-    tab3:set_title("health")
-
-    local tab4, pane4 = win:spawn_tab {
-        workspace = ws,
-        cwd = safir .. "/safir-fusion/scripts",
-        args = { "pwsh.exe" },
-    }
-    tab4:set_title("scripts")
-
-    local tab5, pane5 = win:spawn_tab {
-        workspace = ws,
-        cwd = safir .. "/safir-requirements",
-        args = { "pwsh.exe" },
-    }
-    tab5:set_title("requirements")
+    pane_sub:send_text("cd scripts\r\n")
+    pane_sub:send_text("./SubscribeTo-Tracks.ps1\r\n")
+    local tab_health, pane_health = win:spawn_tab { workspace = ws }
+    tab_health:set_title("health")
+    pane_health:send_text("cd scripts\r\n")
+    pane_health:send_text("./SubscribeTo-Health.ps1\r\n")
+    local tab_scripts, pane_scripts = win:spawn_tab { workspace = ws }
+    tab_scripts:set_title("scripts")
+    local tab_reqs, tabs_pane = win:spawn_tab { workspace = ws }
+    tab_reqs:set_title("requirements")
 
     tab_fusion:activate()
   else
@@ -62,6 +45,6 @@ return {
 	font = wezterm.font_with_fallback({"MesloLGM Nerd Font", "Consolas" }),
 	font_size = 12,
     window_frame = { font_size = 14.0, },
-    window_decorations = "RESIZE",
+    window_decorations = "TITLE",
     default_gui_startup_args = { "--maximize" },
 }
